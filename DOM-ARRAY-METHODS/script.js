34db463d2eada0a1cccf6fecad34a4b5adbfba79
seoyoungsoo <iulove37@naver.com>
2021-04-03 23:52:39 +0900

    Feat: Dom Array Method js
    - 유저 추가, 소유 재산 2배, 1억 이상 보기, 재산 순 내림차순, 재산 합계
      버튼 기능 구현

---
+++ DOM-ARRAY-METHODS/script.js
@@ -0,0 +1,107 @@
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillionairesBtn = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+// Fetch random user and add money
+async function getRandomUser() {
+  const res = await fetch('https://randomuser.me/api');
+  const data = await res.json();
+
+  const user = data.results[0];
+  
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000)
+  }
+
+  addData(newUser);
+}
+// with array function
+// const getRandomUser = () => {
+//   fetch('https://randomuser.me/api')
+//   .then(res => res.json())
+//   .then(data => {
+//     const user = data.results[0];
+
+//     const newUser = {
+//       name: `${user.name.first} ${user.name.last}`,
+//       money: Math.floor(Math.random() * 1000000)
+//     }
+//     console.log(newUser);
+//   })
+// }
+
+// Add new obj to data arr
+const addData = (obj) => {
+  data.push(obj);
+
+  updateDOM();
+}
+
+
+// Double everyones money
+const doubleMoney = _ => {
+  data = data.map((user) => {
+    return { ...user, money: user.money * 2 };
+  });  
+
+  updateDOM();
+}  
+
+// Filter only millionaires
+const showMillionaires = _ => {
+  data = data.filter((user) => user.money > 1000000);
+
+  updateDOM();
+}
+
+// Sort users by richest
+const sortByRichest = _ => {
+  data = data.sort((a, b) => b.money - a.money);
+
+  updateDOM();
+}
+
+// Calculate the total wealth
+const calculateWealth = _ => {
+  const wealth = data.reduce((acc, user) => acc += user.money, 0);
+
+  const wealthEl = document.createElement('div');
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+  main.appendChild(wealthEl);
+}
+
+// Update DOM
+const updateDOM = (providedData = data) => {
+  // Clear main div
+  main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+  providedData.forEach(item => {
+    const element = document.createElement('div');
+    element.classList.add('person');
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
+
+    main.appendChild(element);
+  })
+}
+
+// Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+const formatMoney = (number) => {
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+// Event listeners
+addUserBtn.addEventListener('click', getRandomUser);
+doubleBtn.addEventListener('click', doubleMoney);
+sortBtn.addEventListener('click', sortByRichest);
+showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth)
