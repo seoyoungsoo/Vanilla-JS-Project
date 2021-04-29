2cdffad0d8f591ec48abbd960975ed61f9b12f39
seoyoungsoo <iulove37@naver.com>
2021-04-29 18:47:35 +0900

    Feat: Speak Numbe Guess js

---
+++ SPEAK-NUMBER-GUESS/script.js
@@ -0,0 +1,73 @@
+const msgEl = document.getElementById('msg');
+
+// Generate random number
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
+const randomNum = getRandomNumber();
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+let recognition = new window.SpeechRecognition();
+
+// Start recognition and game
+recognition.start();
+
+// Capture user speak
+const onSpeak = (e) => {
+  const msg = e.results[0][0].transcript;
+  console.log(msg);
+
+  writeMessage(msg);
+  checkNumber(msg);
+}
+
+// Write what user speaks
+const writeMessage = (msg) => {
+  msgEl.innerHTML = `
+    <div>You said: </div>
+    <span class="box">${msg}</span>
+  `;
+}
+
+// Check msg against number
+const checkNumber = (msg) => {
+  const num = +msg;
+
+  // Check if valid number
+  if (Number.isNaN(num)) {
+    msgEl.innerHTML = '<div>That is not a valid number</div>';
+    return;
+  }
+
+  // Check in range
+  if (num > 100 || num < 1) {
+    msgEl.innerHTML += '<div>Number must be between 1 and 100</div>';
+    return;
+  }
+
+  // Check number
+  if (num === randomNum) {
+    document.body.innerHTML = `
+      <h2>Congrats! You hav guessed the number! <br><br> It was ${num}</h2>
+      <button class="play-again" id="play-again">Play Again</button>
+    `;
+  } else if (num > randomNum) {
+    msgEl.innerHTML += '<div>GO LOWER</div>';
+  } else {
+    msgEl.innerHTML += '<div>GO HIGHER</div>';
+  }
+}
+
+// Speak result
+recognition.addEventListener('result', onSpeak);
+
+// End SR service
+recognition.addEventListener('end', () => recognition.start());
+
+document.body.addEventListener('click', () => {
+  if (e.target.id == 'play-again') {
+    window.location.reload();
+  }
+})
